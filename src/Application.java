36820acda8d56d484003f16360f6612b866fdfc0
Denis Eroshevskii <devskjj@gmail.com>
2025-10-01 03:08:28 +0600

    Some refactor.

--- src/Application.java
+++ src/Application.java
@@ -1,2 +1,46 @@
+import models.Car;
+
 public class Application {
+    public static void runApplication() {
+        Car car = new Car();
+        System.out.println(car.getCarId());
+//        List<Integer> numbers = new Random()
+//                .ints(30, 1, 18)
+//                .boxed()
+//                .collect(Collectors.toList());
+//
+//        System.out.println(numbers);
+//
+//        Canvas canvas = new Canvas();
+//
+//        canvas.drawBorder("#");
+////        canvas.setPixel(3, 5, "@");
+//
+//        for(int i = 0; i < numbers.size(); i++) {
+//            for(int j = 0; j < numbers.get(i); j++) {
+//                canvas.setPixel((i + 1) * 2, canvas.getHeight() - j - 2, "*");
+//            }
+//
+//        }
+//
+//        canvas.printTextLine(5, 5, " My canvas ");
+//        System.out.println(canvas);
+//
+//
+//        LocalDateTime now = LocalDateTime.now();
+//
+//        LocalDateTime ldtEnd = now.plusDays(30);
+//
+//        System.out.println(now);
+//        System.out.println(ldtEnd);
+//
+//        int counter = 0;
+//        for (LocalDateTime i = now; i.isBefore(ldtEnd); i = i.plusMinutes(5)) {
+//            System.out.println(i);
+//            counter++;
+//        }
+//
+//        System.out.println(counter);
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,51 +1,6 @@
-import models.Car;
-
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Random;
-import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        Car car = new Car();
-        System.out.println(car.getCarId());
-//        List<Integer> numbers = new Random()
-//                .ints(30, 1, 18)
-//                .boxed()
-//                .collect(Collectors.toList());
-//
-//        System.out.println(numbers);
-//
-//        Canvas canvas = new Canvas();
-//
-//        canvas.drawBorder("#");
-////        canvas.setPixel(3, 5, "@");
-//
-//        for(int i = 0; i < numbers.size(); i++) {
-//            for(int j = 0; j < numbers.get(i); j++) {
-//                canvas.setPixel((i + 1) * 2, canvas.getHeight() - j - 2, "*");
-//            }
-//
-//        }
-//
-//        canvas.printTextLine(5, 5, " My canvas ");
-//        System.out.println(canvas);
-//
-//
-//        LocalDateTime now = LocalDateTime.now();
-//
-//        LocalDateTime ldtEnd = now.plusDays(30);
-//
-//        System.out.println(now);
-//        System.out.println(ldtEnd);
-//
-//        int counter = 0;
-//        for (LocalDateTime i = now; i.isBefore(ldtEnd); i = i.plusMinutes(5)) {
-//            System.out.println(i);
-//            counter++;
-//        }
-//
-//        System.out.println(counter);
-
+        Application.runApplication();
     }
 }
