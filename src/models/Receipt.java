e143b7f9c56c1c9911eab7cf87a0a4610e177bd1
Denis Eroshevskii <devskjj@gmail.com>
2025-10-02 22:13:59 +0600

    Добавил логику работы создания чека относительно времени с 9 утра до 9 вечера. Создал вывод чека. Создал процесс создания чеков относительно условий >30 минут парковки и тд.

--- src/models/Receipt.java
+++ src/models/Receipt.java
@@ -1,6 +1,25 @@
 package models;
 
-public class Receipt {
-    private int cost;
+import java.time.Duration;
 
+public class Receipt {
+    private String carId;
+    private Duration duration;
+    private int cost;
+    private long totalCost;
+
+    public Receipt(String carId, Duration duration, int cost, long totalCost) {
+        this.carId = carId;
+        this.duration = duration;
+        this.cost = cost;
+        this.totalCost = totalCost;
+    }
+
+    @Override
+    public String toString() {
+        return "Чек: " + carId +
+                ", Время стоянки c 9 утра до 9 вечера: " + duration.toMinutes() + " мин" +
+                ", Тариф: " + cost +
+                ", Итого к оплате: " + totalCost;
+    }
 }
