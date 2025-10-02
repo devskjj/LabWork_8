3b86349b10ea6c4cae4f18f8954e5820a4e70668
Denis Eroshevskii <devskjj@gmail.com>
2025-10-03 04:41:59 +0600

    Выборка по среднему проценту загруженности парковки, отношение занятых к свободным местам.

--- src/models/Park.java
+++ src/models/Park.java
@@ -2,13 +2,19 @@ package models;
 
 public class Park {
     private int count;
+    private int initialCount;
 
     public Park(int count) {
         this.count = count;
+        this.initialCount = count;
     }
 
     public int getCount() {
         return count;
+    }
+
+    public int getInitialCount() {
+        return initialCount;
     }
 
     public void setCount(int count) {
