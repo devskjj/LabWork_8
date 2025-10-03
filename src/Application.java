66bf5fd5e2955416dd49757ff7fb6c1653f6874c
Denis Eroshevskii <devskjj@gmail.com>
2025-10-03 13:26:51 +0600

    Предварительно сделал Switch меню функционал для взаимодействия с пользователем.

--- src/Application.java
+++ src/Application.java
@@ -7,6 +7,7 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Application {
     public static void runApplication() {
@@ -56,7 +57,7 @@ public class Application {
         }
         journal.printLog();
 
-        LocalDate chooseDate = LocalDate.now().plusDays(1); // со скана запрашивать число
+        LocalDate chooseDate = LocalDate.now().plusDays(3); // со скана запрашивать число
         Long total = journal.getTotalByDay(chooseDate);
         if (total == null) {
             System.out.printf("За %s нет заработка%n", chooseDate);
@@ -83,4 +84,51 @@ public class Application {
 //        System.out.println(counter);
 
     }
+
+    public static void switchMenu(String choice, Journal journal, Park park, List<Car> cars) {
+        Scanner sc = new Scanner(System.in);
+        switch (choice) {
+            case "1" -> {
+                System.out.println("Введите день: от 1 до 30");
+                String answer = sc.nextLine();
+                LocalDate chooseDate = LocalDate.now().plusDays(Long.parseLong(answer));
+                Long total = journal.getTotalByDay(chooseDate);
+                if (total == null) {
+                    System.out.printf("За %s нет заработка%n", chooseDate);
+                } else {
+                    System.out.printf("Общ. сумма заработка за %s день: %s%n", chooseDate, total);
+                }
+            }
+            case "2" -> journal.printStatistic();
+            case "3" -> journal.printTopTenCars();
+            case "4" -> journal.printLessStayed(30);
+            case "5" -> journal.printCountRatio(park);
+            case "6" -> {
+                System.out.println("Введите номер дня: от 1 до 30");
+                String day = sc.nextLine();
+                LocalDate chooseNewDate = LocalDate.now().plusDays(Long.parseLong(day));
+                journal.printCarsByDate(chooseNewDate);
+                System.out.println("Введите номер часа: от 0 до 24");
+                String hour = sc.nextLine();
+                LocalDateTime start = LocalDateTime.now().withHour(Integer.parseInt(hour)).withMinute(0).withSecond(0);
+                System.out.println("Введите продолжительность: ");
+                String duration = sc.nextLine();
+                LocalDateTime end = start.plusHours(Long.parseLong(duration));
+                journal.printCarsByHour(start, end);
+            }
+            case "7" -> {
+                cars.forEach(car -> System.out.println(car.getCarId()));
+                System.out.print("Введите номер машины: ");
+                String carId = sc.nextLine();
+                journal.printDaysByCarId(carId);
+            }
+            case "8" -> {
+                System.out.println("Выход из программы.");
+                sc.close();
+            }
+            default -> System.out.println("Неверный выбор, пожалуйста, выберите из предложенного списка.");
+        }
+    }
+
+
 }
